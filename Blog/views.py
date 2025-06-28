c2d1c048103dd1562396e435707563452afba073
umeshbogati <bogatiu17@gmail.com>
2025-06-28 13:44:19 +0545

    In views.py we update post_new

--- Blog/views.py
+++ Blog/views.py
@@ -96,3 +96,17 @@ def post_list(request):
     else:
         posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'post':posts})
+
+
+@login_required
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_list')
+    else:
+        form = PostForm()
+    return render(request, 'Blog/post_edit.html', {'form':form})
