e476ece1492cd82deb87ccbfe0ee75b30cc56e8c
umeshbogati <bogatiu17@gmail.com>
2025-06-28 13:30:02 +0545

    Link Post to Author

--- Blog/models.py
+++ Blog/models.py
@@ -8,6 +8,7 @@ class Post(models.Model):
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
     likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
 
     def __str__(self):
         return self.title
